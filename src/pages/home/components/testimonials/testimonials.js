297bcbc1bfcd5c05df520d0920830bf21a8bb764
SepehrAzarm9 <sepehr.azarm369@gmail.com>
2024-10-14 09:28:42 +0330

    fixed some changes

--- src/pages/home/components/testimonials/testimonials.js
+++ src/pages/home/components/testimonials/testimonials.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Slider from "react-slick";
-import Profile1 from "../../../../components/assets/images/profiles/1.png";
-import Profile2 from "../../../../components/assets/images/profiles/2.png";
-import Profile3 from "../../../../components/assets/images/profiles/3.png";
+import Profile1 from "../../../../components/assets/images/profile.png";
+import Profile2 from "../../../../components/assets/images/profile.png";
+import Profile3 from "../../../../components/assets/images/profile.png";
 
 const Testimonials = () => {
   const testimonials = [
